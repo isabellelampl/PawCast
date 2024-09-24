26bf0e0fcc2f7632ef912908424b5606e2ebcd16
isabelrl <isabelrl@uio.no>
2024-09-25 01:02:18 +0200

    init

---
+++ app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/data/ApiKeys.kt
@@ -0,0 +1,12 @@
+package no.uio.ifi.in2000.team19.prosjekt.data
+
+
+
+object ApiKeys {
+    /** Please see ApiKeys for explanation */
+    const val PROXY_KEY = " "
+
+    /** Please see ApiKeys for explanation */
+    const val MAPBOX_ACCESS_TOKEN = " "
+
+}
